010739026a4cf73fb508452be5af90a76d3c9489
Kelly Miller <kellyannemiller@gmail.com>
2017-06-29 07:10:30 -0600

    refactor folders logic

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,7 @@
+$(document).ready(() => {
+  receiveFolders()
+})
+
 $('#button').on('click', () => {
   createFolder()
   createLink()
@@ -11,15 +15,32 @@ const createFolder = () => {
         contentType: 'application/json',
         data: JSON.stringify({ name: folderTitle }),
         dataType: 'json',
-        success: (response) => {console.log(response)}
+        success: (response) => {appendFolders(response)}
+        //call an appendFolders(response)
   })
+}
+
+//const receiveFolders = () => fetch(/api/v1/folders)//loopFolders(response)
+const receiveFolders = () => {
+  fetch('/api/v1/folders')
+    .then(response => response.json())
+    .then(data => loopFolders(data))
+}
+
+const loopFolders = (folders) => {
+  folders.map(folder => appendFolders(folder))
+}
+
+const appendFolders = (folder) => {
+  console.log(folder);
   $('.display-area').append(`
     <section>
-      <button onclick='getLinks()'>${folderTitle}</button>
+      <button onclick='getLinks()'>${folder.name}</button>
       <div class="link-display"></div>
     </section>
   `)
 }
+
 
 const shrinkUrl = () => {
   const urlValue = $('#url').val();
@@ -42,11 +63,18 @@ const createLink = (name, url) => {
         contentType: 'application/json',
         data: JSON.stringify({ link: link }),
         dataType: 'json',
-        success: (response) => {console.log(response)}
+        success: (response) => {appendLinks(response)}
+        //success: (response) => appendLinks(response)
   })
 }
 
-const getLinks = () => {
+//const receiveLinks = (links) == fetch call throwing response into loopLinks(response)
+
+const loopLinks = (links) => {
+  folders.map(link => appendLinks(link))
+}
+
+const appendLinks = (link) => {
   $.get(`/api/links/two`).then((link) => {
     $('.link-display').append(`
       <section>
@@ -54,6 +82,12 @@ const getLinks = () => {
       </section>
     `)
   })
+}
+
+const receiveLinks = () => {
+  fetch('/api/v1/links')
+    .then(response => response.json())
+    .then(data => loopLinks(data))
 }
 
 //// $.get('/api/folders/one').then((message) => {$('.display-area').append(`
