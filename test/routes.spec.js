e41f62f2f3036fb04709d19f7e3434ee165b8928
kamos1 <keji.amos@gmail.com>
2017-06-29 21:12:00 -0600

    add tests for get all folders and create folder

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -10,19 +10,6 @@ const knex = require('../db/knex');
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-  beforeEach((done) => {
-    knex.migrate.rollback()
-    .then(() => {
-      knex.migrate.latest()
-      .then(() => {
-        knex.seed.run()
-        .then(() => {
-          done();
-        })
-      });
-    });
-  });
-
   it('should return the homepage with text', (done) => {
     chai.request(server)
     .get('/')
@@ -39,6 +26,61 @@ describe('Client Routes', () => {
     .end((err, response) => {
       response.should.have.status(404);
       done();
+    });
+  });
+});
+
+describe('API Routes', () => {
+
+  beforeEach((done) => {
+    knex.migrate.rollback()
+    .then(() => {
+      knex.migrate.latest()
+      .then(() => {
+        knex.seed.run()
+        .then(() => {
+          done();
+        })
+      });
+    });
+  });
+
+  describe('GET /api/v1/folders/', () => {
+    it('should return all of the folders', (done) => {
+      chai.request(server)
+      .get('/api/v1/folders/')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(2);
+        done();
+      });
+    });
+  });
+
+  describe('POST /api/v1/folders', () => {
+    it('should create a new folder', (done) => {
+      chai.request(server)
+      .post('/api/v1/folders')
+      .send({
+        name: 'google',
+      })
+      .end((err, response) => {
+        response.should.have.status(201);
+        response.body.should.be.a('object');
+        response.body.should.have.property('id');
+        response.body.id.should.equal(3);
+        chai.request(server)
+        .get('/api/v1/folders/')
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(3);
+          done();
+        });
+      });
     });
   });
 });
