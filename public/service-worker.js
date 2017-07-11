69a4c5730b968bb6860f9f77264b5eb222f5c888
Kelly Miller <kellyannemiller@gmail.com>
2017-07-11 10:45:53 -0600

    add activate service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,6 +18,20 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
-    });
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  let cacheWhitelist = ['assets-v1'];
+
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(kelList.map(key => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
