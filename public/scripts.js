d918a689b70a716af6fe2991e00d382e96b96eaa
Kelly Miller <kellyannemiller@gmail.com>
2017-07-11 10:35:17 -0600

    add service worker

--- public/scripts.js
+++ public/scripts.js
@@ -233,3 +233,15 @@ $('.display-area').on('click', '#sort-least-pop', function() {
 
   sortByTheLeast(folderId, element);
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () =>{
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(registration => {
+        console.log('reg', registration);
+      })
+      .catch(error => {
+        console.log('Failure');
+      });
+  });
+}
